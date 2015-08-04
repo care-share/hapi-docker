2424e9c07e34d63368db8700479a8dcd5d766ce7
Joe Portner <jportner@mitre.org>
2015-08-04 15:11:13 -0400

    Database backup and restoration
    Added initial FHIR resources; 1 Organization and 24 Patients.
    Added script to back up the FHIR derby database from a container.
    Modified webapp config file and Dockerfile to restore the database
    when the container runs.
    Added an initial database backup (we can continue to update this on
    an as-needed basis).

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,12 @@ ADD docker/tiller /etc/tiller
 # Copy TLS keystore, if any
 COPY docker/tls ${CATALINA_HOME}/conf/tls
 
+# Replace the database configuration (triggers restoration of database backup)
+COPY docker/WEB-INF/hapi-fhir-server-database-config.xml ${CATALINA_HOME}/webapps/hapi-fhir/WEB-INF/
+
+# Restore the database backup
+ADD docker/derby_backup.tar.gz /tmp/derby_backup/
+
 # Run Tiller
 CMD ["/usr/local/bin/tiller" , "-v"]
 
---
+++ docker/backup_database.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: backup_database.sh <docker-container-id>"
+  exit 1
+fi
+
+SRC=/usr/local/tomcat/jpaserver_derby_files
+BKDIR=/tmp/derby_backup
+BKFILE=/tmp/derby_backup.tar.gz
+
+# create the derby backup
+docker exec -t $1 bash -c "rm -rf $BKDIR \
+&& rm -f $BKFILE \
+&& cp -r $SRC $BKDIR \
+&& tar czf $BKFILE -C $BKDIR ."
+#&& rm $BKDIR/*.lck \
+
+# copy the backup from the container
+docker cp $1:$BKFILE ./
+
